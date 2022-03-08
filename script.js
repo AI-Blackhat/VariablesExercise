b24f056f38308149a965b2d43fb01bd92fa1a111
“AI-Blackhat” <todd.mau@.gmail.com>
2022-03-08 01:50:59 -0500

    Variables Exercise with Bonus material

---
+++ script.js
@@ -0,0 +1,69 @@
+// VARIABLES EXERCISE
+// 1
+// const
+
+// 2
+let num = 20;
+
+// 3
+num = 15;
+
+//4
+num = num + 7;
+// or
+// num += 7;
+
+// 5
+num--;
+
+// 6
+num = "This is not a number";
+
+// 7
+let blank;
+// Note: No =
+// Will get a blank space, an empty string if you use =
+// Adding an = would be defining it and giving it a value.
+
+// 8
+console.log(blank);
+
+// 9
+blank = null;
+
+// Bonus
+// 1
+const quarter = "0.25", dime = "0.10", nickel = "0.05", penny = "0.01";
+// Need quotation marks around each value, otherwise values will add to $ 0.41.
+
+// const [quarter, dime, nickel, penny] = [0.25, 0.10, 0.05, 0.01];
+// like an array
+
+// 2
+console.log(quarter + dime + nickel + penny);
+
+// or
+
+console.log(quarter, dime, nickel, penny);
+
+// 3
+let letters = "qwertyuiop";
+
+// 4
+console.log(letters.length);
+console.log(letters[9]); //p
+
+//or
+
+console.log(letters.slice(9));
+
+//or
+
+console.log(letters.slice(-1));
+
+// or
+
+console.log(letters[letters.length - 1]);
+
+
+
